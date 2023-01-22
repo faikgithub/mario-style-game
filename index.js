e9ceb80bfe2fff2b9948e6523a7a072b1e2f1ab9
faikgithub <faikalpbusiness@gmail.com>
2023-01-22 15:26:12 +0100

    gravity

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+const gravity = 1.5
+
 class Player {
   constructor() {
     this.position = {
@@ -25,7 +27,26 @@ class Player {
       position.y, this.width, this.height
     )
   }
+
+  update(){
+    this.draw()
+    this.position.y += this.velocity.y
+    if(this.position.y +  this.height + this.velocity.y <= canvas.height)
+      this.velocity.y += gravity
+      
+    else this.velocity.y= 0
+
+
+  }
 }
 
 const player = new Player()
-player.draw()
+player.update()
+
+function animate(){
+requestAnimationFrame(animate)
+c.clearRect(0, 0, canvas.width, canvas.height)
+player.update()
+}
+
+animate()
