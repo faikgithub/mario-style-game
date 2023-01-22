13c547d18556bfb60379ebf1b35f9fe56aa7e73e
faikgithub <faikalpbusiness@gmail.com>
2023-01-22 15:15:08 +0100

    player

--- index.js
+++ index.js
@@ -2,4 +2,30 @@ const canvas = document.querySelector('canvas')
 
 const c = canvas.getContext('2d')
 
-console.log(c)
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100
+    }
+    this.velocity = {
+      x: 0,
+      y: 1
+    }
+    this.width = 30
+    this.height = 30
+  }
+
+  draw() {
+    c.fillStyle = 'red'
+    c.fillRect(this.position.x, this.
+      position.y, this.width, this.height
+    )
+  }
+}
+
+const player = new Player()
+player.draw()
